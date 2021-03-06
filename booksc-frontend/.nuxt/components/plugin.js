870efa7b4d3ad495a6a06b0d4d095f2b2b60868f
IceSlam <mainguy@mail.ru>
2021-03-06 21:20:16 +0300

    Rafactored footer to Bootstrap-Vue

--- booksc-frontend/.nuxt/components/plugin.js
+++ booksc-frontend/.nuxt/components/plugin.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 
 const components = {
   Logo: () => import('../..\\components\\Logo.vue' /* webpackChunkName: "components/logo" */).then(c => c.default || c),
+  HomeSocial: () => import('../..\\components\\home\\social.vue' /* webpackChunkName: "components/home-social" */).then(c => c.default || c),
   MainFooter: () => import('../..\\components\\main\\Footer.vue' /* webpackChunkName: "components/main-footer" */).then(c => c.default || c),
   MainHeader: () => import('../..\\components\\main\\Header.vue' /* webpackChunkName: "components/main-header" */).then(c => c.default || c),
   MainNavbar: () => import('../..\\components\\main\\Navbar.vue' /* webpackChunkName: "components/main-navbar" */).then(c => c.default || c),
-  HomeSocial: () => import('../..\\components\\home\\social.vue' /* webpackChunkName: "components/home-social" */).then(c => c.default || c),
   ServicesHeading: () => import('../..\\components\\services\\Heading.vue' /* webpackChunkName: "components/services-heading" */).then(c => c.default || c),
   ServicesHeadingCategory: () => import('../..\\components\\services\\HeadingCategory.vue' /* webpackChunkName: "components/services-heading-category" */).then(c => c.default || c)
 }
--- booksc-frontend/components/main/Footer.vue
+++ booksc-frontend/components/main/Footer.vue
@@ -43,113 +43,110 @@
         <b-col
           md="3"
           class="mx-auto"
-        ></b-col>
-        <hr class="clearfix w-100 d-md-none">
-        <b-col
-          md="3"
-          class="mx-auto"
-        ></b-col>
-        <b-col
-          md="3"
-          class="mx-auto"
-        ></b-col>
-      </b-row>
-    </b-container>
-    <div class="container text-center text-md-left">
-      <div class="row">
-        <div class="col-md-3 mx-auto">
-          <h5 class="font-weight-bold text-uppercase mt-3 mb-4 wow slideInUp" data-wow-duration="2s">
-            Book-Service <br>ТД Весна
+        >
+          <h5
+            class="font-weight-bold text-uppercase mt-3 mb-4 text-white">
+            Book-Service <br> ТД Весна
           </h5>
-          <p class="is_contacts-item wow slideInUp" data-wow-duration="2s">
-            <i class="fas fa-map-marked" aria-hidden="true"></i>
+          <p class="is-contacts-item">
+            <i class="fas fa-map-marked" aria-hidden="true" />
             <span class="d-none" itemprop="postalCode">241037</span>
-            <span itemprop="addressLocality">Брянск</span>, <span itemprop="streetAddress">III Интернационала ул., 17А</span>, ост. Почта, ТД Весна, 4 этаж
+            <span itemprop="addressLocality">Брянск</span>,
+            <span itemprop="streetAddress">III Интернационала ул., 17А</span>,
+            ост. Почта, ТД Весна, 4 этаж
           </p>
-          <p class="is_contacts-item wow slideInUp" data-wow-duration="2s">
-            <i class="fas fa-phone" aria-hidden="true"></i>
+          <p class="is-contacts-item">
+            <i class="fas fa-phone" aria-hidden="true" />
             тел.:
-            <a href="tel:+79307202222" class="is_contacts-item-link">
+            <a href="tel:+79307202222" class="is-contacts-item-link">
                 <span itemprop="telephone">
                     +7-930-720-2222
                 </span>
             </a>
           </p>
-          <p class="is_contacts-item wow slideInUp" data-wow-duration="2s">
+          <p class="is-contacts-item">
             <i class="fas fa-envelope" aria-hidden="true"></i>
             email:
-            <a href="mailto:help@booksc.ru" class="is_contacts-item-link">
+            <a href="mailto:help@booksc.ru" class="is-contacts-item-link">
               <span itemprop="email">help@booksc.ru</span>
             </a>
           </p>
-          <p class="is_contacts-item wow slideInUp" data-wow-duration="2s">
+          <p class="is-contacts-item">
             <i class="far fa-calendar-alt"></i>
             ПН-СБ: 10:00 - 19:00 / ВС: 10:00 - 18:00
           </p>
-        </div>
+        </b-col>
         <hr class="clearfix w-100 d-md-none">
-        <div class="col-md-3 mx-auto" />
-        <div class="col-md-3 mx-auto text-right wow slideInRight" data-wow-duration="2s" style="line-height:2;">
-          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">
+        <b-col
+          md="3"
+          class="mx-auto offset-md-3 text-right"
+          style="line-height:2;"
+        >
+          <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-white">
             Ссылки
           </h5>
           <ul class="list-unstyled">
             <li>
-              <a href="#!">Главная</a>
+              <NLink to="/">
+                Главная
+              </NLink>
             </li>
             <li>
-              <a href="#!">Услуги и цены</a>
+              <NLink to="/services">
+                Услуги и цены
+              </NLink>
             </li>
             <li>
-              <a href="#!">Магазин BSMarket</a>
-            </li>
-            <li>
-              <a href="#!" data-toggle="modal" data-target="#exampleModalCenter">Статус ремонта</a>
-            </li>
-            <li>
-              <a href="#!">О компании</a>
-            </li>
-            <li>
-              <a href="#!">Контакты</a>
-            </li>
-            <li>
-              <a data-toggle="collapse" data-target="#collapseOne2" href="#!">
-                СЦ на карте
+              <a
+                href="https://bookparts.ru/"
+                target="_blank"
+              >
+                Магазин BookParts
               </a>
             </li>
+            <li>
+              <NLink to="/contacts">
+                Контакты
+              </NLink>
+            </li>
+            <li>
+              <b-button
+                v-b-toggle.collapsed-ya-map
+                class="map-collapse-btn"
+              >
+                СЦ на карте
+              </b-button>
+            </li>
           </ul>
-        </div>
-      </div>
-    </div>
-    <div class="" id="map-accordeon" name="map-accordeon">
-      <div class="card z-depth-0">
-        <div
-          id="collapseOne2"
-          class="collapse"
-          aria-labelledby="headingOne2"
-          data-parent="#map-accordeon"
-        >
-          <div class="card-body map-card-body">
-            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A76ea0ff14f25dccb0001d250726fa6efa283b055a34a618a96877072ceef3fd7&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
-          </div>
-        </div>
-      </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <div id="map-accordeon">
+      <b-card
+        class="z-depth-0"
+        style="background-color: transparent; border: none;"
+      >
+        <b-collapse id="collapsed-ya-map">
+          <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A76ea0ff14f25dccb0001d250726fa6efa283b055a34a618a96877072ceef3fd7&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+        </b-collapse>
+      </b-card>
     </div>
     <div class="footer-copyright text-center py-3">
       <p class="is_main-footer-copy">
-        © 2014 - 2020 Copyright
+        © 2014 - 2021 Copyright
         <a href="https://booksc.ru"> Сеть сервисных центров Book-Service</a> <br>
         ИП Моисеенко К.А. ИНН: 323307422295 ОГРНИП: 314325624800051 <br>
         Копирование и распространение материалов с данного сайта только с разрешения администрации сайта и с указанием ссылки на первоисточник. <br> Цены, указанные на сайте, не являются публичной офертой. Для уточнения цен необходимо обратиться в офис компании.
         <br>
         Developed by
-        <a href="https://vk.com/iceslam">
+        <a
+          href="https://iceslam.ru"
+          target="_blank"
+        >
           IceSlam
         </a>
         and based on
-        <a href="https://modx.com/">
-          Modx Revolution
-        </a>
+        Nuxt.js and Strapi
       </p>
     </div>
   </footer>
@@ -171,6 +168,29 @@ export default {
   color: #ffffff;
 }
 .is-contacts-item-link {
+  color: #ffffff;
+}
+.list-unstyled li a {
+  color: #ffffff;
+}
+.card-body {
+  padding: 0;
+}
+.map-collapse-btn,
+.map-collapse-btn:active,
+.map-collapse-btn:checked,
+.map-collapse-btn:hover,
+.map-collapse-btn:focus{
+  padding: 0;
+  background-color: transparent;
+  border: none;
+}
+.footer-copyright {
+  color: rgba(255,255,255,.6);
+  background-color: rgba(0,0,0,.2);
+  font-weight: 600;
+}
+.footer-copyright a {
   color: #ffffff;
 }
 </style>
