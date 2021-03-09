745cf7da3611f2837726717db015abc6d79c6428
IceSlam <mainguy@mail.ru>
2021-03-09 03:02:31 +0300

    Changed New goods Store in frontend

--- site/store/index.js
+++ site/store/index.js
@@ -15,7 +15,7 @@ const store = () => new Vuex.Store({
   },
   actions: {
     GET_NEW_GOODS_FROM_API ({ commit }) {
-      return axios('http://localhost:1337/new-goods', {
+      return axios('http://localhost:1894/new-goods', {
         method: 'GET'
       })
         .then((response) => {
