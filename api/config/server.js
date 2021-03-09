c187a429cc70e5c0944611ccdd5a07b4ff9e46c7
IceSlam <mainguy@mail.ru>
2021-03-09 15:35:33 +0300

    Fixed API addresses and Admin URL

--- api/config/server.js
+++ api/config/server.js
@@ -5,5 +5,8 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a5ef6810b89043ccf719dfc82069f054'),
     },
+    url: '/dashboard',
+    serveAdminPanel: 'true',
+    host: 'https://dev.api.booksc.ru'
   },
 });
