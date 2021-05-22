a90d58076ab58ed228c1fabb0934113d9348a862
Paul Khlopkov <mainguy@mail.ru>
2021-05-22 12:15:56 +0300

    Fixed bugs

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -26,7 +26,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/uikit.js', ssr: false }
+    { src: '~/plugins/uikit.js', ssr: false },
+    { src: '~/plugins/custom.js', ssr: false }
   ],
 
   pageTransition: 'ispage',
