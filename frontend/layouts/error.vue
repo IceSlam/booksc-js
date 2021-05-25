d262f840e0c8dd0719e5c97b3770293721a99ab5
Paul Khlopkov <mainguy@mail.ru>
2021-05-25 21:43:29 +0300

    Removed unused layouts

---
+++ frontend/layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrap container">
+    <h1 v-if="error.statusCode === 404">Страница не найдена</h1>
+    <h1 v-else>Произошла ошибка</h1>
+    {{ error }}
+    <NuxtLink to="/">На главную</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  props: ['error'],
+  layout: 'error',
+  transition: 'ispage',
+  head () {
+    return {
+      title: 'Страница не найдена - Сеть сервисных центров Book-Service'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ frontend/layouts/homepage.vue
@@ -0,0 +1,65 @@
+<template>
+  <main class="wrapper">
+    <LazyHydrate when-idle>
+      <Header
+        :header-data="THEME_SETTINGS"
+      />
+    </LazyHydrate>
+    <LazyHydrate when-idle>
+      <Navbar
+        :navbar-data="THEME_SETTINGS"
+      />
+    </LazyHydrate>
+    <nuxt />
+    <LazyHydrate when-idle>
+      <Footer
+        :footer-data="THEME_SETTINGS"
+      />
+    </LazyHydrate>
+    <v-scroll-to-top class="to-top-btn" />
+  </main>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import LazyHydrate from 'vue-lazy-hydration'
+
+export default {
+  components: {
+    LazyHydrate,
+    Header: () => import('@/components/main/Header.vue'),
+    Navbar: () => import('@/components/main/Navbar'),
+    Footer: () => import('@/components/main/Footer')
+  },
+  head () {
+    return {
+      title: 'Сеть сервисных центров Book-Service',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Ремонт телефонов, планшетов, ноутбуков и компьютеров, техники Apple и устройств IQOS'
+        }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: './img/logotype.png' }]
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'THEME_SETTINGS'
+    ])
+  },
+  mounted () {
+    this.GET_THEME_SETTINGS_FROM_API()
+  },
+  methods: {
+    ...mapActions([
+      'GET_THEME_SETTINGS_FROM_API'
+    ]),
+  }
+}
+</script>
+<style>
+</style>
